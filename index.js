0ef6101085824c880926823441a3c0b3f2de6947
amisha patel <vermaamisha785@gmail.com>
2021-12-10 15:55:44 +0530

    comment

---
+++ index.js
@@ -0,0 +1,6 @@
+const userData = [
+    {name : 'amisha', sirname: 'patel'},
+    {name : 'manas', sirname: 'thakur'},
+    {name : 'prachi', sirname: 'gupta'},
+    {name : 'shivansh', sirname: 'patil'}
+]
